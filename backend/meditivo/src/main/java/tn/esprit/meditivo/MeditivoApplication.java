a6014a05c310da7f2ecf47e6b58102709ef0a3f2
unknown <mohamedaziz.salemzitoun@esprit.tn>
2025-02-28 04:41:06 +0100

    Added backend project

---
+++ backend/meditivo/src/main/java/tn/esprit/meditivo/MeditivoApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.meditivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditivoApplication.class, args);
+	}
+
+}
